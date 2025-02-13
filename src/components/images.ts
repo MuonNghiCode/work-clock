47c769c05db00b93ff516ddc74c2e823bba887da
Quang Vinh <156314258+quangvinh-hub@users.noreply.github.com>
2025-02-13 16:40:13 +0700

    update-image.ts

---
+++ src/components/images.ts
@@ -0,0 +1,7 @@
+const Images = {
+    // Logo: require("./assets/logo.png"), // Nếu dùng require
+    // AvatarDefault: "https://example.com/default-avatar.png", // Ảnh từ URL
+    Logo: "src/assets/images/logo3.png", // Nếu dùng URL trực tiếp
+};
+  
+  export default Images;
