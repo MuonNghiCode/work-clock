e508cedfee8bdbcd5dee28a7fa67f9be411f8efe
MuonNghiCode <minhquanpm1610@gmail.com>
2025-02-13 16:46:46 +0700

    update header homepage

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import MainHeader from "../Header/MainHeader/MainHeader";
 import Footer from "../Footer/Footer";
 const MainLayout: React.FC = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <>
       <div className="flex flex-col h-screen">
-        <header className="relative bg-white text-black text-center p-4 shadow-[0_10px_5px_rgba(255,145,77,0.2)]">
-          <MainHeader />
-        </header>
-        <main className="flex-1 bg-gray-200 p-4">
+        {isHomePage ? (
+          <header className="relative bg-white text-black text-center p-1 ">
+            <MainHeader />
+          </header>
+        ) : (
+          <header className="relative bg-white text-black text-center p-4 shadow-orange-header">
+            <MainHeader />
+          </header>
+        )}
+        <main className="flex-1 bg-white p-4">
           <Outlet />
         </main>
-        <footer className="z-10 bg-white text-black text-center p-4 shadow-[0_-10px_5px_rgba(255,145,77,0.2)]">
-          <Footer />
-        </footer>
+        {!isHomePage && (
+          <footer className="z-10 bg-white text-black text-center p-4 shadow-orange-footer">
+            <Footer />
+          </footer>
+        )}
       </div>
     </>
   );
