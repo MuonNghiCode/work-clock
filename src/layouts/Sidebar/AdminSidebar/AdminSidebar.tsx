99254bfff149d308630fbb5dc52604a95f2fcc05
MuonNghiCode <minhquanpm1610@gmail.com>
2025-02-20 09:48:12 +0700

    update sidebar

--- src/layouts/Sidebar/AdminSidebar/AdminSidebar.tsx
+++ src/layouts/Sidebar/AdminSidebar/AdminSidebar.tsx
@@ -79,7 +79,8 @@ const AdminSidebar: React.FC = () => {
             }}
             className="flex items-center justify-start gap-3 mt-10"
           >
-            <Icons.LogOut /> LOG OUT
+            <Icons.LogOut className="w-8 h-8" />
+            {isSidebarExpanded && "Log out"}
           </NavLink>
         </div>
       </div>
