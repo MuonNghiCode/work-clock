98780f0783d6926f25784df375650b1537bb7d2e
hoangnd <hoangndse184443@fpt.edu.vn>
2025-02-24 18:09:26 +0700

    responsive

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -148,136 +148,132 @@ const LoginPage: React.FC = () => {
 
       {/* Login Form */}
       <div
-        className={`lg:w-2/3 w-screen m-0 lg:m-0 lg:h-4/5  h-4/6 flex border border-black rounded-[30px] bg-white z-10 ${
+        className={`lg:w-2/3 w-full m-0 lg:m-0 lg:h-4/5  h-5/6 flex border border-black rounded-[30px] bg-white z-10 ${
           isChangePassword ? "hidden" : ""
         }`}
       >
-        <div className="w-full  items-center lg:flex hidden">
+        <div className=" items-center  lg:flex hidden">
           <img
             src={Images.Background3}
             alt="Background"
-            className="w-full h-12/13 scale-x-112 object-contain translate-x-[-60px]"
+            className="w-full scale-x-120  h-12/13 translate-x-14 mr-16 object-contain "
           />
         </div>
-        <div className="lg:w-1/2 w-1/3 lg:flex flex-col space-y-4 relative">
+        <div className="lg:w-1/2 w-full m-1  lg:flex flex-col space-y-4 relative">
           <img
             src={Images.Logo}
             alt="Logo"
-            className="w-28 lg:w-40 mx-auto absolute top-3 left-10 "
+            className="w-28 lg:w-40 mx-auto absolute lg:top-0 lg:right-0 "
           />
 
-          <h1 className="text-3xl lg:text-4xl text-center left-11 lg:translate-x-[-40px] absolute lg:top-30 top-20 whitespace-nowrap">
-            Welcome User
-          </h1>
-          <div className=" h-full flex flex-col justify-center absolute lg:top-0 top-[-20px] left-11 lg:right-20 lg:items-end">
-            <form
-              onSubmit={handleLoginSubmit}
-              className="h-full relative lg:top-17 lg:left-10 flex flex-col justify-center items-center space-y-6"
+          <form
+            onSubmit={handleLoginSubmit}
+            className="w-full h-full relative top-10 flex flex-col justify-center items-center space-y-4 lg:space-y-5"
+          >
+            <h1 className="text-3xl lg:text-4xl text-center whitespace-nowrap">
+              Welcome User
+            </h1>
+            {/* Email Field */}
+            <div className="relative w-full lg:w-80">
+              <span
+                className={`absolute left-2 top-2 text-gray-500 transition-all pointer-events-none ${
+                  email || isEmailFocused
+                    ? "text-xs -translate-y-6 px-2 text-blue-500"
+                    : "text-base"
+                }`}
+              >
+                Email
+              </span>
+              <input
+                id="Email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                onFocus={() => setIsEmailFocused(true)}
+                onBlur={() => setIsEmailFocused(false)}
+                className="w-full h-10 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              {errors.email && (
+                <p className="text-red-500 text-sm absolute">{errors.email}</p>
+              )}
+              <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-700">
+                <Icons.Email />
+              </span>
+            </div>
+
+            {/* Password Field */}
+            <div className="relative w-full lg:w-80">
+              <span
+                className={`absolute left-2 top-2 text-gray-500 transition-all pointer-events-none ${
+                  password || isPasswordFocused
+                    ? "text-xs -translate-y-6 px-2 text-blue-500"
+                    : "text-base"
+                }`}
+              >
+                Password
+              </span>
+              <input
+                id="Password"
+                type={isPasswordVisible ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                onFocus={() => setIsPasswordFocused(true)}
+                onBlur={() => setIsPasswordFocused(false)}
+                className="w-full h-10 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <span
+                onClick={() => setIsPasswordVisible(!isPasswordVisible)}
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-900 cursor-pointer"
+              >
+                {isPasswordVisible ? <Icons.Unlock /> : <Icons.Lock />}
+              </span>
+              {errors.password && (
+                <p className="text-red-500 text-sm absolute">
+                  {errors.password}
+                </p>
+              )}
+            </div>
+
+            {/* Buttons */}
+            <button
+              type="submit"
+              className="mt-5 lg:mt-2 lg:w-50 w-4/6  h-10 px-4 py-2 bg-brand-grandient text-white rounded-lg cursor-pointer transition-all hover:scale-105 focus:outline-none"
             >
-              {/* Email Field */}
-              <div className="relative w-full lg:w-80">
-                <span
-                  className={`absolute left-2 top-2 text-gray-500 transition-all pointer-events-none ${
-                    email || isEmailFocused
-                      ? "text-xs -translate-y-6 px-2 text-blue-500"
-                      : "text-base"
-                  }`}
-                >
-                  Email
-                </span>
-                <input
-                  id="Email"
-                  type="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  onFocus={() => setIsEmailFocused(true)}
-                  onBlur={() => setIsEmailFocused(false)}
-                  className="w-full h-10 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                {errors.email && (
-                  <p className="text-red-500 text-sm absolute">
-                    {errors.email}
-                  </p>
-                )}
-                <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-700">
-                  <Icons.Email />
-                </span>
-              </div>
-
-              {/* Password Field */}
-              <div className="relative w-full lg:w-80">
-                <span
-                  className={`absolute left-2 top-2 text-gray-500 transition-all pointer-events-none ${
-                    password || isPasswordFocused
-                      ? "text-xs -translate-y-6 px-2 text-blue-500"
-                      : "text-base"
-                  }`}
-                >
-                  Password
-                </span>
-                <input
-                  id="Password"
-                  type={isPasswordVisible ? "text" : "password"}
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  onFocus={() => setIsPasswordFocused(true)}
-                  onBlur={() => setIsPasswordFocused(false)}
-                  className="w-full h-10 p-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                />
-                <span
-                  onClick={() => setIsPasswordVisible(!isPasswordVisible)}
-                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-900 cursor-pointer"
-                >
-                  {isPasswordVisible ? <Icons.Unlock /> : <Icons.Lock />}
-                </span>
-                {errors.password && (
-                  <p className="text-red-500 text-sm absolute">
-                    {errors.password}
-                  </p>
-                )}
-              </div>
-
-              {/* Buttons */}
-              <button
-                type="submit"
-                className="mt-5 lg:mt-2 w-50 h-10 px-4 py-2 bg-brand-grandient text-white rounded-lg cursor-pointer transition-all hover:scale-105 focus:outline-none"
-              >
-                Login
-              </button>
-              <button
-                type="button"
-                onClick={() => setIsChangePassword(true)}
-                className="w-50 h-10 px-4 py-2 bg-white text-black border-amber-500 border rounded-lg cursor-pointer transition-all hover:scale-105 focus:outline-none"
-              >
-                Forgot Password
-              </button>
-            </form>
-          </div>
+              Login
+            </button>
+            <button
+              type="button"
+              onClick={() => setIsChangePassword(true)}
+              className="lg:w-50 w-4/6 h-10 px-4 py-2 bg-white text-black border-amber-500 border rounded-lg cursor-pointer transition-all hover:scale-105 focus:outline-none"
+            >
+              Forgot Password
+            </button>
+          </form>
         </div>
       </div>
 
       {/* Change Password Form */}
       <div
-        className={`lg:w-2/3 w-full lg:h-4/5 h-4/6 flex items-center border border-black rounded-[30px] bg-white z-20 ${
+        className={`lg:w-2/3 w-full lg:h-4/5 h-5/6 flex items-center border border-black rounded-[30px] bg-white z-20 ${
           !isChangePassword ? "hidden" : ""
         }`}
       >
         <div className="w-full h-full flex border border-black rounded-[30px] bg-white z-10 flex-col lg:flex-row">
           {/* Left Section - Form */}
-          <div className="lg:w-1/2 w-full flex flex-col space-y-4 relative p-10">
+          <div className="lg:w-1/2 w-full h-1/2 flex flex-col space-y-4 relative p-10">
             <img
               src={Images.Logo}
               alt="Logo"
-              className="w-28 lg:w-40 mx-auto absolute top-3 left-10  "
+              className="w-28 lg:w-40 mx-auto absolute top-1 left-0  "
             />
-            <h1 className="text-3xl lg:text-4xl text-center absolute top-20 whitespace-nowrap">
-              Change Password
-            </h1>
 
             <form
               onSubmit={handleChangePasswordSubmit}
-              className="h-full flex flex-col justify-center items-center space-y-6  mt-30"
+              className="h-3/6 flex flex-col justify-center items-center lg:space-y-5 space-y-4 mt-40 lg:mt-50"
             >
+              <h1 className="text-3xl lg:text-4xl text-center absolute top-20 whitespace-nowrap">
+                Change Password
+              </h1>
               {/* Email Field */}
               <div className="relative w-full lg:w-80">
                 <span
@@ -345,27 +341,26 @@ const LoginPage: React.FC = () => {
 
               <button
                 type="submit"
-                className="mt-5 lg:mt-2 w-50 h-10 px-4 py-2 bg-brand-grandient text-white rounded-lg cursor-pointer transition-all hover:scale-105 focus:outline-none"
+                className=" lg:mt-2 lg:w-50 w-4/6 h-10 px-4 py-2 bg-brand-grandient text-white rounded-lg cursor-pointer transition-all hover:scale-105 focus:outline-none"
               >
                 Confirm
               </button>
               <button
                 type="button"
                 onClick={() => setIsChangePassword(false)}
-                className="w-50 h-10 px-4 py-2 bg-white text-black border-amber-500 border rounded-lg cursor-pointer transition-all hover:scale-105 focus:outline-none"
+                className="lg:w-50 w-4/6 lg:h-10 h-10 px-4 py-2 bg-white text-black border-amber-500 border rounded-lg cursor-pointer transition-all hover:scale-105 focus:outline-none"
               >
                 Back
               </button>
             </form>
           </div>
-
-          {/* Right Section - Image */}
-          <div className="w-full  items-center lg:flex hidden">
+          <div className=" items-center lg:flex hidden">
             <img
               src={Images.Background4}
-              className="w-full h-12/13 scale-x-112 object-contain translate-x-5"
+              className="w-full scale-x-120  h-12/13 translate-x-7  object-contain "
             />
           </div>
+          {/* Right Section - Image */}
         </div>
       </div>
     </div>
