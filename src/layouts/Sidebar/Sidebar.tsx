4e4f3574a8c27ec3ca6368e11cbb5742207f6fe9
Le Huu Hai Tri <TriLHHSE183828@fpt.edu.vn>
2025-02-25 16:47:52 +0700

    Update layout Edit Profile

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -122,27 +122,18 @@ const Sidebar: React.FC = () => {
                   key={index}
                   to={item.path}
                   className={({ isActive }) =>
-                    `flex items-center justify-start gap-3 px-4 py-2 rounded-md transition-all duration-300 ${isActive
+                    `flex items-center justify-start gap-3 mt-5 px-4 py-2 rounded-md transition-all duration-300 transform ${isSidebarOpen
+                      ? "justify-start gap-3 px-4"
+                      : "justify-center px-2"
+                    } py-2 rounded-md transition-all duration-300 overflow-hidden text-ellipsis whitespace-nowrap ${isActive
                       ? isSidebarOpen
-                        ? "bg-brand-gradient text-white translate-x-9"
-                        : "bg-brand-gradient text-white translate-x-3"
+                        ? "rounded-l-2xl bg-brand-gradient text-white translate-x-9"
+                        : "rounded-l-2xl bg-brand-gradient text-white translate-x-9"
                       : "bg-transparent"
-                      } ${hoveredIndex !== null && hoveredIndex !== index
-                        ? "text-gray-500 blur-[2px] "
-                        : "text-black"
-                      }`
-                      `flex items-center justify-start gap-3 mt-5 px-4 py-2 rounded-md transition-all duration-300 transform ${isSidebarOpen
-                        ? "justify-start gap-3 px-4"
-                        : "justify-center px-2"
-                      } py-2 rounded-md transition-all duration-300 overflow-hidden text-ellipsis whitespace-nowrap ${isActive
-                        ? isSidebarOpen
-                          ? "rounded-l-2xl bg-brand-gradient text-white translate-x-9"
-                          : "rounded-l-2xl bg-brand-gradient text-white translate-x-9"
-                        : "bg-transparent"
-                      } ${hoveredIndex !== null && hoveredIndex !== index
-                        ? "text-gray-500 blur-[2px]"
-                        : "text-black"
-                      } ${isSidebarOpen ? "max-w-[180px]" : "max-w-[50px]"}`
+                    } ${hoveredIndex !== null && hoveredIndex !== index
+                      ? "text-gray-500 blur-[2px]"
+                      : "text-black"
+                    } ${isSidebarOpen ? "max-w-[180px]" : "max-w-[50px]"}`
                   }
                   onMouseEnter={() => setHoveredIndex(index)}
                   onMouseLeave={() => setHoveredIndex(null)}
@@ -173,12 +164,8 @@ const Sidebar: React.FC = () => {
               setShowConfirm(true);
             }}
             className={`flex items-center justify-start gap-3 mt-5 px-4 py-2 rounded-md transition-all duration-300 transform ${isSidebarOpen
-              ? "hover:bg-red-600 hover:text-white hover:translate-x-9"
-              : "hover:bg-red-600 hover:text-white hover:translate-x-3"
-              }`}
-            className={`flex items-center justify-start gap-3 mt-5 px-4 py-2 rounded-md transition-all duration-300 transform ${isSidebarOpen
-                ? "rounded-l-2xl hover:bg-red-600 hover:text-white hover:translate-x-9"
-                : "rounded-l-2xl hover:bg-red-600 hover:text-white hover:translate-x-3"
+              ? "rounded-l-2xl hover:bg-red-600 hover:text-white hover:translate-x-9"
+              : "rounded-l-2xl hover:bg-red-600 hover:text-white hover:translate-x-3"
               }`}
           >
             <Icons.LogOut className="w-8 h-8" />
@@ -186,7 +173,6 @@ const Sidebar: React.FC = () => {
           </NavLink>
         </div>
       </div>
-
       {showConfirm && (
         <div className="fixed inset-0 flex items-center justify-center bg-brand-orange-light backdrop-blur-sm z-50">
           <div className="bg-white p-6 rounded-lg shadow-lg text-center w-80">
@@ -216,3 +202,4 @@ const Sidebar: React.FC = () => {
 };
 
 export default Sidebar;
+
