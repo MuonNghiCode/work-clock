fbab44f310fc8c15a4498fefb45862bccfb4124a
danh-pham235 <phamdanh0305@gmail.com>
2025-03-04 09:41:28 +0700

    resolve problem

--- src/services/claimService.ts
+++ src/services/claimService.ts
@@ -1,20 +1,20 @@
 import { post, get } from "./apiService";
 import { API_CONTANTS } from "../constants/apiContants";
-import { ClaimSearchResponse, ClaimDetailResponse, SearchCondition, PageInfoRequest } from "../types/ClaimType";
+import { SearchCondition, PageInfoRequest, ClaimItem, PageInfo } from "../types/ClaimType";
+import { ResponseModel } from "../models/ResponseModel";
 
 export const getClaimerSearch = async (
   searchCondition: SearchCondition,
   pageInfo: PageInfoRequest
-): Promise<ClaimSearchResponse> => {
-  const response = await post(API_CONTANTS.CLAIMS.CLAIMER_SEARCH, { 
+): Promise<ResponseModel<{ pageData: ClaimItem[], pageInfo: PageInfo }>> => {
+  const response = await post<{ pageData: ClaimItem[], pageInfo: PageInfo }>(API_CONTANTS.CLAIMS.CLAIMER_SEARCH, { 
     searchCondition, 
     pageInfo 
   });
-  return response as ClaimSearchResponse;
+  return response;
 };
 
-
-export const getClaimDetail = async (id: string): Promise<ClaimDetailResponse> => {
-  const response = await get(`${API_CONTANTS.CLAIMS.CLAIM_DETAIL}/${id}`); 
-  return response as ClaimDetailResponse;
+export const getClaimDetail = async (id: string): Promise<ResponseModel<ClaimItem>> => {
+  const response = await get<ClaimItem>(`${API_CONTANTS.CLAIMS.CLAIM_DETAIL}/${id}`); 
+  return response;
 };
