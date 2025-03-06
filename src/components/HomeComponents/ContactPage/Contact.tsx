5b74862f720bca0fd40e628b6de891d107d79be3
minh <tylum123@gmail.com>
2025-03-06 09:52:02 +0700

    contact email

--- src/components/HomeComponents/ContactPage/Contact.tsx
+++ src/components/HomeComponents/ContactPage/Contact.tsx
@@ -1,7 +1,78 @@
 import React from "react";
+import emailjs from "@emailjs/browser";
 import Icons from "../../icon";
+import { toast } from "react-toastify";
 
 const Contact: React.FC = () => {
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const templateParams = {
+      first_name: formData.get("first_name"),
+      last_name: formData.get("last_name"),
+      email: formData.get("email"),
+      phone: formData.get("phone"),
+      message: formData.get("message"),
+    };
+
+    emailjs
+      .send(
+        "service_l9qbtvf",
+        "template_sg5shq7",
+        templateParams,
+        "EV-tXrDmzrcr5DOwr"
+      )
+      .then((response) => {
+        console.log("Email sent successfully!", response.status, response.text);
+        toast.success("Email sent successfully!");
+      })
+      .catch((error) => {
+        console.error("Failed to send email:", error);
+        toast.error("Failed to send email");
+      });
+    e.currentTarget.reset();
+  };
+
+  const sendRequestEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const templateParams = {
+      first_name: formData.get("first_name"),
+      last_name: formData.get("last_name"),
+      email: formData.get("email"),
+      phone: formData.get("phone"),
+      message: formData.get("message"),
+      cc_email: "tylum123@gmail.com, meme91men@gmail.com",
+    };
+
+    emailjs
+      .send(
+        "service_05enpzu",
+        "template_661t2y1",
+        templateParams,
+        "EV-tXrDmzrcr5DOwr"
+      )
+      .then((response) => {
+        console.log(
+          "Request email sent successfully!",
+          response.status,
+          response.text
+        );
+      })
+      .catch((error) => {
+        console.error("Failed to send request email:", error);
+      });
+    e.currentTarget.reset();
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    sendEmail(e);
+    sendRequestEmail(e);
+  };
+
   return (
     <div className="min-h-screen px-4 py-6 md:px-0">
       <h1 className="text-3xl font-bold text-center md:text-5xl text-gradient-color">
@@ -14,19 +85,19 @@ const Contact: React.FC = () => {
             <h2 className="mb-4 text-xl font-bold md:text-2xl">
               Contact Information
             </h2>
-            <p className="mb-6">Say something to start a live chat!</p>
+            <p className="mb-6">Start a live chat with us!</p>
             <div className="space-y-4">
               <div className="flex items-center justify-start space-x-3 md:justify-start">
                 <span>
                   <Icons.Phone />
                 </span>
-                <span>+1012 3456 789</span>
+                <span>+84 1012 3456 789</span>
               </div>
               <div className="flex items-center justify-start space-x-3 md:justify-start">
                 <span>
                   <Icons.Mail />
                 </span>
-                <span>demo@gmail.com</span>
+                <span>contact@workclock.com</span>
               </div>
               <div className="flex items-start justify-start space-x-3 md:justify-start">
                 <span>
@@ -40,55 +111,75 @@ const Contact: React.FC = () => {
             </div>
           </div>
 
-          {/* Right Side - Form */}
           <div className="p-6 md:p-8">
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={handleSubmit}>
               <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
                 <div>
                   <label className="block text-gray-700">First Name</label>
-                  <input
-                    type="text"
-                    className="w-full p-2 border-b border-gray-300 focus:outline-none"
-                    required
-                  />
+                  <div className="flex items-center space-x-2">
+                    <Icons.User className="text-gray-500" />
+                    <input
+                      type="text"
+                      name="first_name"
+                      placeholder="First Name"
+                      className="w-full p-2 border-b border-gray-300 focus:outline-none"
+                      required
+                    />
+                  </div>
                 </div>
                 <div>
                   <label className="block text-gray-700">Last Name</label>
-                  <input
-                    type="text"
-                    className="w-full p-2 border-b border-gray-300 focus:outline-none"
-                    required
-                  />
+                  <div className="flex items-center space-x-2">
+                    <input
+                      type="text"
+                      name="last_name"
+                      placeholder="Last Name"
+                      className="w-full p-2 border-b border-gray-300 focus:outline-none"
+                      required
+                    />
+                  </div>
                 </div>
               </div>
 
               <div>
                 <label className="block text-gray-700">Email</label>
-                <input
-                  type="email"
-                  className="w-full p-2 border-b border-gray-300 focus:outline-none"
-                  required
-                />
+                <div className="flex items-center space-x-2">
+                  <Icons.Mail className="text-gray-500" />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    className="w-full p-2 border-b border-gray-300 focus:outline-none"
+                    required
+                  />
+                </div>
               </div>
 
               <div>
                 <label className="block text-gray-700">Phone Number</label>
-                <input
-                  type="tel"
-                  className="w-full p-2 border-b border-gray-300 focus:outline-none"
-                  required
-                  defaultValue="+1 012 3456 789"
-                />
+                <div className="flex items-center space-x-2">
+                  <Icons.Phone className="text-gray-500" />
+                  <input
+                    type="tel"
+                    name="phone"
+                    placeholder="Phone Number"
+                    className="w-full p-2 border-b border-gray-300 focus:outline-none"
+                    required
+                  />
+                </div>
               </div>
 
               <div>
                 <label className="block text-gray-700">Message</label>
-                <textarea
-                  className="w-full p-2 border-b border-gray-300 focus:outline-none"
-                  rows={3}
-                  placeholder="Write your message.."
-                  required
-                ></textarea>
+                <div className="flex items-center space-x-2">
+                  <textarea
+                    name="message"
+                    placeholder="Write your message.."
+                    className="w-full p-2 border-b border-gray-300 focus:outline-none"
+                    rows={3}
+                    required
+                  ></textarea>
+                </div>
               </div>
 
               <div className="flex justify-center md:justify-end">
