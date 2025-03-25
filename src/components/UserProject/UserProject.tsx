01dff370301f8c42d80e52c2d6b58d8dc159f814
Quang Vinh <156314258+quangvinh-hub@users.noreply.github.com>
2025-03-25 09:18:46 +0700

    update-project-filter

--- src/components/UserProject/UserProject.tsx
+++ src/components/UserProject/UserProject.tsx
@@ -3,8 +3,6 @@ import { Pagination } from "antd";
 import { getAllProject } from "../../services/projectService"; // Import the new function
 import { useUser } from "../../contexts/UserContext";
 import { formatDate } from "../../utils/formatDate";
-import { useUser } from "../../contexts/UserContext";
-import { formatDate } from "../../utils/formatDate";
 
 interface ProjectInfo {
   key: string;
@@ -32,7 +30,6 @@ const UserProject = () => {
           project_end_date: "",
           is_delete: false,
           user_id: user?._id || "",
-          user_id: user?._id || "",
         },
         pageInfo: { pageNum, pageSize },
       });
@@ -41,8 +38,6 @@ const UserProject = () => {
       let data = response.data.pageData.map((item: any) => ({
         key: item._id,
         projectName: item.project_name,
-        startDate: formatDate(new Date(item.project_start_date), "DD/MM/YYYY"),
-        endDate: formatDate(new Date(item.project_end_date), "DD/MM/YYYY"),
         startDate: formatDate(new Date(item.project_start_date), "DD/MM/YYYY"),
         endDate: formatDate(new Date(item.project_end_date), "DD/MM/YYYY"),
         status: item.project_status,
