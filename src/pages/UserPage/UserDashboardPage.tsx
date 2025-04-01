653932047073be412c358ee4a50bb8190657623a
hoangnd <hoangndse184443@fpt.edu.vn>
2025-04-01 10:30:58 +0700

    pull dev

--- src/pages/UserPage/UserDashboardPage.tsx
+++ src/pages/UserPage/UserDashboardPage.tsx
@@ -3,8 +3,14 @@ import { Button } from "antd";
 import ModalAddNewClaim from "../../components/UserComponents/ModalAddNewClaim";
 import { motion } from "framer-motion";
 import Icons from "../../components/icon";
-import { getAllClaims } from "../../services/claimService";
+import { getClaimerSearch } from "../../services/claimService";
 import { useNavigate } from "react-router-dom";
+import { ResponseModel } from "../../models/ResponseModel";
+import {
+  ClaimItem,
+  PageInfoRequest,
+  SearchCondition,
+} from "../../types/ClaimType";
 
 const fadeInScaleUp = {
   hidden: { opacity: 0, scale: 0.8 },
@@ -54,10 +60,13 @@ const StatCard = ({
 );
 
 const UserDashboardPage = () => {
+  const [currentPage] = useState(1);
+  const [pageSize] = useState(3);
   const [isOpenModalAddNewClaim, setIsOpenModalAddNewClaim] = useState(false);
   const [claimsData, setClaimsData] = useState<ClaimRequest[]>([]);
   const [loading, setLoading] = useState(true);
-  const [ClaimsCount, setClaimsCount] = useState(0);
+  const [setClaimsCount] = useState(0);
+  const [MainClaimsCount, setMainClaimsCount] = useState(0);
   const [draftClaimsCount, setDraftClaimsCount] = useState(0);
   const [pendingClaimsCount, setPendingClaimsCount] = useState(0);
   const [successClaimsCount, setSuccessClaimsCount] = useState(0);
@@ -66,42 +75,84 @@ const UserDashboardPage = () => {
   const handleOpenModalAddNewClaim = () => setIsOpenModalAddNewClaim(true);
   const handleCloseModalAddNewClaim = () => setIsOpenModalAddNewClaim(false);
 
-  const fetchClaims = async () => {
+  const fetchMainClaims = async () => {
     try {
-      const response = await getAllClaims({
-        searchCondition: {
+      const response = await getClaimerSearch(
+        {
           keyword: "",
           claim_status: "",
           is_delete: false,
-          project_start_date: "",
-          project_end_date: "",
+          claim_start_date: "",
+          claim_end_date: "",
         },
-        pageInfo: { pageNum: 1, pageSize: 30 },
-      });
-      const data = response.data.pageData.map((item: any) => ({
-        key: item._id,
-        claimname: item.claim_name,
-        project: item.project_info?.project_name,
-        start_date: new Date(item.claim_start_date).toLocaleDateString("en-US"),
-        end_date: new Date(item.claim_end_date).toLocaleDateString("en-US"),
-        total_work_time: item.total_work_time,
-        timeFrom: new Date(item.claim_start_date).toLocaleTimeString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-        }),
-        timeTo: new Date(item.claim_end_date).toLocaleTimeString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          hour12: false,
-        }),
-        status: item.claim_status,
-        approval_name: item.approval_name,
-      }));
-      setClaimsData(data);
-      setClaimsCount(data.length);
+        { pageNum: 1, pageSize: 30 }
+      );
+      setMainClaimsCount(response.data.pageData.length);
     } catch (error) {
       console.log(error);
+    }
+  };
+
+  const fetchClaims = async () => {
+    setLoading(true);
+    try {
+      const searchCondition: SearchCondition = {
+        keyword: "",
+        claim_status: "",
+        claim_start_date: "",
+        claim_end_date: "",
+        is_delete: false,
+      };
+      const pageInfo: PageInfoRequest = { pageNum: currentPage, pageSize };
+
+      const response: ResponseModel<{
+        pageData: ClaimItem[];
+        pageInfo: PageInfoRequest;
+      }> = await getClaimerSearch(searchCondition, pageInfo);
+
+      if (response.success) {
+        const claims = response.data.pageData.map((item) => ({
+          key: item._id,
+          claimname: item.claim_name,
+          project: item.project_info?.project_name || "N/A",
+          start_date: item.claim_start_date
+            ? new Date(item.claim_start_date).toLocaleDateString("en-US")
+            : "N/A",
+          end_date: item.claim_end_date
+            ? new Date(item.claim_end_date).toLocaleDateString("en-US")
+            : "N/A",
+          total_work_time: item.total_work_time || "0",
+          timeFrom: item.claim_start_date
+            ? new Date(item.claim_start_date).toLocaleTimeString("en-US", {
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: false,
+              })
+            : "N/A",
+          timeTo: item.claim_end_date
+            ? new Date(item.claim_end_date).toLocaleTimeString("en-US", {
+                hour: "2-digit",
+                minute: "2-digit",
+                hour12: false,
+              })
+            : "N/A",
+          status: item.claim_status || "Unknown",
+          // approval_name: item.approval_name || "N/A",
+        }));
+
+        // Sort claims by `start_date` in descending order
+        const claims = claims.sort((a, b) => {
+          const dateA = new Date(a.start_date).getTime();
+          const dateB = new Date(b.start_date).getTime();
+          return dateB - dateA;
+        });
+
+        setClaimsData(claims);
+      } else {
+        console.warn("API returned no data for this search:", searchCondition);
+      }
+    } catch (error) {
+      console.error("Error fetching claims:", error);
     } finally {
       setLoading(false);
     }
@@ -109,16 +160,16 @@ const UserDashboardPage = () => {
 
   const fetchDraftClaims = async () => {
     try {
-      const response = await getAllClaims({
-        searchCondition: {
+      const response = await getClaimerSearch(
+        {
+          keyword: "",
           claim_status: "Draft",
           is_delete: false,
-          keyword: "",
-          project_start_date: "",
-          project_end_date: "",
+          claim_start_date: "",
+          claim_end_date: "",
         },
-        pageInfo: { pageNum: 1, pageSize: 30 },
-      });
+        { pageNum: 1, pageSize: 30 }
+      );
       setDraftClaimsCount(response.data.pageData.length);
     } catch (error) {
       console.log(error);
@@ -127,16 +178,16 @@ const UserDashboardPage = () => {
 
   const fetchSuccessClaims = async () => {
     try {
-      const response = await getAllClaims({
-        searchCondition: {
+      const response = await getClaimerSearch(
+        {
+          keyword: "",
           claim_status: "Paid",
           is_delete: false,
-          keyword: "",
-          project_start_date: "",
-          project_end_date: "",
+          claim_start_date: "",
+          claim_end_date: "",
         },
-        pageInfo: { pageNum: 1, pageSize: 30 },
-      });
+        { pageNum: 1, pageSize: 30 }
+      );
       setSuccessClaimsCount(response.data.pageData.length);
     } catch (error) {
       console.log(error);
@@ -145,16 +196,16 @@ const UserDashboardPage = () => {
 
   const fetchPendingClaims = async () => {
     try {
-      const response = await getAllClaims({
-        searchCondition: {
+      const response = await getClaimerSearch(
+        {
+          keyword: "",
           claim_status: "Pending Approval",
           is_delete: false,
-          keyword: "",
-          project_start_date: "",
-          project_end_date: "",
+          claim_start_date: "",
+          claim_end_date: "",
         },
-        pageInfo: { pageNum: 1, pageSize: 10 },
-      });
+        { pageNum: 1, pageSize: 30 }
+      );
       setPendingClaimsCount(response.data.pageData.length);
     } catch (error) {
       console.log(error);
@@ -163,6 +214,7 @@ const UserDashboardPage = () => {
 
   useEffect(() => {
     fetchClaims();
+    fetchMainClaims();
     fetchDraftClaims();
     fetchPendingClaims();
     fetchSuccessClaims();
@@ -183,7 +235,7 @@ const UserDashboardPage = () => {
         <StatCard
           icon={<Icons.FormIcon className="text-3xl text-blue-600" />}
           label="Created Claims"
-          value={ClaimsCount}
+          value={MainClaimsCount}
           bgColor="bg-gradient-to-b from-blue-300 to-blue-100"
           textColor="text-black-900 font-bold"
         />
@@ -233,64 +285,57 @@ const UserDashboardPage = () => {
                 </td>
               </tr>
             ) : claimsData.length > 0 ? (
-              claimsData
-                .sort(
-                  (a, b) =>
-                    new Date(b.start_date).getTime() -
-                    new Date(a.start_date).getTime()
-                )
-                .slice(0, 3)
-                .map((item) => (
-                  <tr
-                    key={item.key}
-                    className="h-[70px] bg-white text-center hover:shadow-brand-orange rounded-2xl"
-                  >
-                    <td className="request-table-cell px-4 py-2 rounded-l-2xl">
-                      {item.claimname}
-                    </td>
-                    <td className="request-table-cell px-4 py-2">
-                      {item.start_date}
-                    </td>
-                    <td className="request-table-cell px-4 py-2">
-                      {item.end_date}
-                    </td>
-                    <td className="request-table-cell px-4 py-2">
-                      <div className="request-table-hours flex flex-col items-center">
-                        <span className="font-semibold text-[#FF914D]">
-                          {item.total_work_time} hours
-                        </span>
-                      </div>
-                    </td>
-                    <td className="px-4 py-2">{item.status}</td>
-                    <td className="request-table-cell px-4 py-2 rounded-r-2xl">
-                      <div className="request-table-actions flex justify-center gap-2">
-                        <Button
-                          className="!border-none"
-                          onClick={() => navigate("/user/request")}
-                          title="View Details"
+              claimsData.slice(0, 3).map((item) => (
+                <tr
+                  key={item.key}
+                  className="h-[70px] bg-white text-center hover:shadow-brand-orange rounded-2xl"
+                >
+                  <td className="request-table-cell px-4 py-2 rounded-l-2xl">
+                    {item.claimname}
+                  </td>
+                  <td className="request-table-cell px-4 py-2">
+                    {item.start_date}
+                  </td>
+                  <td className="request-table-cell px-4 py-2">
+                    {item.end_date}
+                  </td>
+                  <td className="request-table-cell px-4 py-2">
+                    <div className="request-table-hours flex flex-col items-center">
+                      <span className="font-semibold text-[#FF914D]">
+                        {item.total_work_time} hours
+                      </span>
+                    </div>
+                  </td>
+                  <td className="px-4 py-2">{item.status}</td>
+                  <td className="request-table-cell px-4 py-2 rounded-r-2xl">
+                    <div className="request-table-actions flex justify-center gap-2">
+                      <Button
+                        className="!border-none"
+                        onClick={() => navigate("/user/request")}
+                        title="View Details"
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="18"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="#FF914D"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="lucide lucide-chart-column"
                         >
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="18"
-                            height="18"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="#FF914D"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="lucide lucide-chart-column"
-                          >
-                            <path d="M3 3v16a2 2 0 0 0 2 2h16"></path>
-                            <path d="M18 17V9"></path>
-                            <path d="M13 17V5"></path>
-                            <path d="M8 17v-3"></path>
-                          </svg>
-                        </Button>
-                      </div>
-                    </td>
-                  </tr>
-                ))
+                          <path d="M3 3v16a2 2 0 0 0 2 2h16"></path>
+                          <path d="M18 17V9"></path>
+                          <path d="M13 17V5"></path>
+                          <path d="M8 17v-3"></path>
+                        </svg>
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))
             ) : (
               <tr>
                 <td colSpan={6} className="text-center py-4">
